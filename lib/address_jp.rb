0368f515c4133c65d6581fbd56146042f48593a9
鈴木竜太 <django.contrib.auth.models@gmail.com>
2017-12-16 14:09:29 +0900

    add city class

--- lib/address_jp.rb
+++ lib/address_jp.rb
@@ -1,6 +1,7 @@
 require 'address_jp/version'
 require 'address_jp/util'
 require 'address_jp/prefecture'
+require 'address_jp/city'
 
 require 'yaml'
 
---
+++ lib/address_jp/city.rb
@@ -0,0 +1,23 @@
+module AddressJp
+  class City
+    attr_reader :id, :name
+
+    def initialize(id:, name:, prefecture_id:)
+      @id = id.to_i
+      @prefecture_id = prefecture_id.to_i
+      @name = name
+    end
+
+    class << self
+      def all
+        AddressJp::Util.load_master_data(:cities).map do |hash|
+          AddressJp::City.new(**hash)
+        end
+      end
+
+      def regex
+        /(#{all.map(&:name).join('|')})/
+      end
+    end
+  end
+end
