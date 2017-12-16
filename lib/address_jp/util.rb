7081e8b48808f38d5e5452d3860bc32d869f909a
鈴木竜太 <django.contrib.auth.models@gmail.com>
2017-12-16 13:56:21 +0900

    dump cities, wards to yml

--- lib/address_jp/util.rb
+++ lib/address_jp/util.rb
@@ -7,5 +7,10 @@ module AddressJp
     def self.load_master_data(file_name)
       YAML.load_file("#{root}/lib/address_jp/data/#{file_name}.yml")
     end
+
+    def self.dump_yaml(object, file_name)
+      file_path = "#{root}/lib/address_jp/data/#{file_name}.yml"
+      YAML.dump(object, File.open(file_path, 'w'))
+    end
   end
 end
