a837e85ccc157a182ee0810e53e3153dd5c22d79
鈴木竜太 <django.contrib.auth.models@gmail.com>
2017-12-23 16:16:03 +0900

    county spec

---
+++ spec/address_jp/county_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+describe County do
+  let(:county) { County.new(id: 2300, prefecture_id: 2, name: '東津軽郡') }
+
+  describe '#name' do
+    subject { county.name }
+    it { is_expected.to eq '東津軽郡' }
+  end
+
+  describe '#prefecture' do
+    subject { county.prefecture }
+    it 'get expected prefecture' do
+      expect(subject.id).to eq 2
+      expect(subject.name).to eq '青森県'
+    end
+  end
+end
